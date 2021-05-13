625244ce1902ceaedaef26d794b692a038000559
shiang-hue <81691780+shiang-hue@users.noreply.github.com>
2021-05-13 22:12:24 +0800

    Update rtmp_netstream.go

--- rtmp/rtmp_netstream.go
+++ rtmp/rtmp_netstream.go
@@ -1,11 +1,11 @@
 package rtmp
 
 import (
-	"../avformat"
-	"../config"
-	"../hls"
-	"../mpegts"
-	"../util"
+	"github.com/shiang-hue/gortmp/avformat"
+	"github.com/shiang-hue/gortmp/config"
+	"github.com/shiang-hue/gortmp/hls"
+	"github.com/shiang-hue/gortmp/mpegts"
+	"github.com/shiang-hue/gortmp/util"
 	"bytes"
 	"fmt"
 	"io"
